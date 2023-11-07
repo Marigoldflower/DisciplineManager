4e2747105ebd6f89fb7ad96f7cb65aeed1d6bc08
Marigold <marigold410@naver.com>
2023-11-07 16:58:41 +0900

    Initial Commit

---
+++ DisciplineManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisciplineManager
+//
+//  Created by 황홍필 on 2023/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
