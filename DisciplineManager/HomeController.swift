f6e2c6d4dd33a761358449e5984b9f182a1cd9a8
Marigold <marigold410@naver.com>
2023-11-08 16:07:46 +0900

    DisciplineManager 캘린더 UI 및 Font 구성

--- DisciplineManager/HomeController.swift
+++ DisciplineManager/HomeController.swift
@@ -14,35 +14,17 @@ final class HomeController: UIViewController {
     private let calendar = FSCalendar()
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-        setCalendarUI()
         setCalendarDelegate()
-    }
+        for family in UIFont.familyNames {
+            print(family)
 
-    private func setCalendarUI() {
-        setCalendarTextColor()
-        hideCalendarText()
-        setCalendarAsWeek()
-    }
-    
-    private func setCalendarTextColor() {
-        calendar.appearance.titleDefaultColor = .black
-        calendar.appearance.titleWeekendColor = .systemPink
-        calendar.appearance.headerTitleColor = .systemPink
-        calendar.appearance.weekdayTextColor = .orange
-    }
-    
-    private func hideCalendarText() {
-        calendar.appearance.headerMinimumDissolvedAlpha = 0
-        
-        
-    }
-    
-    private func setCalendarAsWeek() {
-        calendar.scope = .week
+            for sub in UIFont.fontNames(forFamilyName: family) {
+                 print("====> \(sub)")
+            }
+        }
     }
     
     private func setCalendarDelegate() {
@@ -50,13 +32,14 @@ final class HomeController: UIViewController {
         calendar.dataSource = self
     }
     
-
 }
 
 extension HomeController: ViewDrawable {
+    // MARK: - protocol 구현부
     func configureUI() {
         setBackgroundColor()
         setAutolayout()
+        setCalendarUI()
     }
     
     func setBackgroundColor() {
@@ -70,9 +53,47 @@ extension HomeController: ViewDrawable {
             make.leading.equalTo(view.snp.leading)
             make.trailing.equalTo(view.snp.trailing)
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.height.equalTo(220)
+            make.height.equalTo(240)
         }
     }
+    
+    // MARK: - 나머지 UI 구현부
+    private func setCalendarUI() {
+        setCalendarTextColor()
+        hideCalendarText()
+        setCalendarAsWeek()
+        setCalendarFonts()
+    }
+    
+    private func setCalendarTextColor() {
+        calendar.appearance.titleDefaultColor = .black
+        calendar.appearance.titleWeekendColor = .systemPink
+        calendar.appearance.headerTitleColor = .systemPink
+        calendar.appearance.weekdayTextColor = .orange
+    }
+    
+    private func hideCalendarText() {
+        hideCalendarHeaderSideText()
+        hideCalendarHeaderTitleText()
+    }
+    
+    private func hideCalendarHeaderSideText() {
+        calendar.appearance.headerMinimumDissolvedAlpha = 0
+    }
+    
+    private func hideCalendarHeaderTitleText() {
+        calendar.headerHeight = 0
+    }
+    
+    private func setCalendarAsWeek() {
+        calendar.scope = .week
+    }
+    
+    private func setCalendarFonts() {
+        calendar.appearance.titleFont = UIFont(name: "LINESeedSansKR-Bold", size: 15.0)
+        calendar.appearance.weekdayFont = UIFont(name: "LINESeedSansKR-Regular", size: 15.0)
+    }
+    
 }
 
 extension HomeController: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
